719c5eb4dd288652491bcdefe3ea6ae51cabd6ca
hzbdesigner-macbook pro <hzbdesigner@gmail.com>
2013-04-02 17:11:58 +0800

    填充+list样式~这次lsit花了很长时间~需提高

--- protected/views/wangluo/xiaoyuan.php
+++ protected/views/wangluo/xiaoyuan.php
@@ -1,8 +1,101 @@
-<?php
-$this->breadcrumbs=array(
-	'Wangluo'=>array('wangluo/index'),
-	'Xiaoyuan',
-);?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+<div class="row-fluid wangluolists">
+	<div class="span6">
 
-<p>You may change the content of this page by modifying the file <tt><?php echo __FILE__; ?></tt>.</p>
+		<div class="row-fluid">
+			<div class="thumbnail span5 ">
+			 <img src="http://dummyimage.com/800x600/ddd/686a82.gif&text=placeholder+image" alt="placeholder+image">
+			 <i class=""><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets-frontend/img/icon-video.png"></i>
+			</div>
+			<div class="span7 content">
+				<div class="title"><a href="#">视频标题视频标题</a></div>
+				<p class="meta"><span> <i class=" icon-film"></i> 浏览:5</span><span> <i class="icon-user"></i> 作者:admin</span></p>
+				<p class="destext">视频简介，描述视频简介，描述视频简介，描述视频简介，描述</p>
+			</div>
+		</div>
+	</div>
+	<div class="span6">
+
+		<div class="row-fluid">
+			<div class="thumbnail span5 ">
+			 <img src="http://dummyimage.com/800x600/ddd/686a82.gif&text=placeholder+image" alt="placeholder+image">
+			 <i class=""><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets-frontend/img/icon-video.png"></i>
+			</div>
+			<div class="span7 content">
+				<div class="title"><a href="#">视频标题视频标题</a></div>
+				<p class="meta"><span> <i class=" icon-film"></i> 浏览:5</span><span> <i class="icon-user"></i> 作者:admin</span></p>
+				<p class="destext">视频简介，描述视频简介，描述视频简介，描述视频简介，描述</p>
+			</div>
+		</div>
+	</div>
+	<div class="span6">
+
+		<div class="row-fluid">
+			<div class="thumbnail span5 ">
+			 <img src="http://dummyimage.com/800x600/ddd/686a82.gif&text=placeholder+image" alt="placeholder+image">
+			 <i class=""><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets-frontend/img/icon-video.png"></i>
+			</div>
+			<div class="span7 content">
+				<div class="title"><a href="#">视频标题视频标题</a></div>
+				<p class="meta"><span> <i class=" icon-film"></i> 浏览:5</span><span> <i class="icon-user"></i> 作者:admin</span></p>
+				<p class="destext">视频简介，描述视频简介，描述视频简介，描述视频简介，描述</p>
+			</div>
+		</div>
+	</div>
+	<div class="span6">
+
+		<div class="row-fluid">
+			<div class="thumbnail span5 ">
+			 <img src="http://dummyimage.com/800x600/ddd/686a82.gif&text=placeholder+image" alt="placeholder+image">
+			 <i class=""><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets-frontend/img/icon-video.png"></i>
+			</div>
+			<div class="span7 content">
+				<div class="title"><a href="#">视频标题视频标题</a></div>
+				<p class="meta"><span> <i class=" icon-film"></i> 浏览:5</span><span> <i class="icon-user"></i> 作者:admin</span></p>
+				<p class="destext">视频简介，描述视频简介，描述视频简介，描述视频简介，描述</p>
+			</div>
+		</div>
+	</div>
+	<div class="span6">
+
+		<div class="row-fluid">
+			<div class="thumbnail span5 ">
+			 <img src="http://dummyimage.com/800x600/ddd/686a82.gif&text=placeholder+image" alt="placeholder+image">
+			 <i class=""><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets-frontend/img/icon-video.png"></i>
+			</div>
+			<div class="span7 content">
+				<div class="title"><a href="#">视频标题视频标题</a></div>
+				<p class="meta"><span> <i class=" icon-film"></i> 浏览:5</span><span> <i class="icon-user"></i> 作者:admin</span></p>
+				<p class="destext">视频简介，描述视频简介，描述视频简介，描述视频简介，描述</p>
+			</div>
+		</div>
+	</div>
+	<div class="span6">
+
+		<div class="row-fluid">
+			<div class="thumbnail span5 ">
+			 <img src="http://dummyimage.com/800x600/ddd/686a82.gif&text=placeholder+image" alt="placeholder+image">
+			 <i class=""><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets-frontend/img/icon-video.png"></i>
+			</div>
+			<div class="span7 content">
+				<div class="title"><a href="#">视频标题视频标题</a></div>
+				<p class="meta"><span> <i class=" icon-film"></i> 浏览:5</span><span> <i class="icon-user"></i> 作者:admin</span></p>
+				<p class="destext">视频简介，描述视频简介，描述视频简介，描述视频简介，描述</p>
+			</div>
+		</div>
+	</div>
+	<div class="span6">
+
+		<div class="row-fluid">
+			<div class="thumbnail span5 ">
+			 <img src="http://dummyimage.com/800x600/ddd/686a82.gif&text=placeholder+image" alt="placeholder+image">
+			 <i class=""><img src="<?php echo Yii::app()->request->baseUrl; ?>/assets-frontend/img/icon-video.png"></i>
+			</div>
+			<div class="span7 content">
+				<div class="title"><a href="#">视频标题视频标题</a></div>
+				<p class="meta"><span> <i class=" icon-film"></i> 浏览:5</span><span> <i class="icon-user"></i> 作者:admin</span></p>
+				<p class="destext">视频简介，描述视频简介，描述视频简介，描述视频简介，描述</p>
+			</div>
+		</div>
+	</div>
+
+</div>
