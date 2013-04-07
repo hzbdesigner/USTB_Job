656d14c22dc959c66e48e6d0932878c221ae2f93
hzbdesigner-macbook pro <hzbdesigner@gmail.com>
2013-04-07 10:04:36 +0800

    index fix

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -309,18 +309,7 @@ EOD;
 EOD;
 						}
 					?>
-					<div class="span10">
-						<div class="thumbnail"><a href="#"><img src="http://dummyimage.com/800x600/ddd/686a82.gif&text=placeholder+image" alt="placeholder+image"></a></div>
-						<div class="des">
-							<h1 class=""><a href="#">老师名字</a></h1>
-						</div>
-					</div>
-					<div class="span10">
-						<div class="thumbnail"><a href="#"><img src="http://dummyimage.com/800x600/ddd/686a82.gif&text=placeholder+image" alt="placeholder+image"></a></div>
-						<div class="des">
-							<h1 class=""><a href="#">老师名字</a></h1>
-						</div>
-					</div>
+
 				</div>
 			</div>
 		</div>
