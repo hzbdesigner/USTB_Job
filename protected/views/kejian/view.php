226c4992109aedb36064c077f72244953a179c35
hzbdesigner-macbook pro <hzbdesigner@gmail.com>
2013-05-01 20:13:07 +0800

    弹窗
    下载和删除的弹窗效果

--- protected/views/kejian/view.php
+++ protected/views/kejian/view.php
@@ -2,9 +2,20 @@
 		echo $model->content;
 
 ?>
-<p>
-	课件下载
-	<a href="#">
-		filename.filetype
-	</a>
+<p style="margin-top:20px;">
+		<a class="btn btn-primary" data-toggle="modal" href="#myModal">下载视频</a>
+		<div class="modal fade" id="myModal">
+			<div class="modal-header">
+				<a class="close" data-dismiss="modal">&times;</a>
+				<h3>视频下载</h3>
+			</div>
+			<div class="modal-body">
+				<p>您是否要下载视频</p>
+			</div>
+			<div class="modal-footer">
+				<a href="#" class="btn btn-primary">下载</a>
+				<a href="#" class="btn" data-dismiss="modal">取消</a>
+			</div>
+		</div>
+
 </p>
