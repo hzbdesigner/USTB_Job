912d2662130ee46448c929c541c99a523ba286ce
hzbdesigner-macbook pro <hzbdesigner@gmail.com>
2013-05-09 21:10:42 +0800

    删除favicon

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -32,17 +32,6 @@
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 
-	<!-- Le fav and touch icons -->
-	<link rel="shortcut icon" href="<?php echo Yii::app()->
-	request->baseUrl; ?>/assets-frontend/ico/favicon.ico">
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo Yii::app()->
-	request->baseUrl; ?>/assets-frontend/ico/apple-touch-icon-144-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo Yii::app()->
-	request->baseUrl; ?>/assets-frontend/ico/apple-touch-icon-114-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo Yii::app()->
-	request->baseUrl; ?>/assets-frontend/ico/apple-touch-icon-72-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" href="<?php echo Yii::app()->
-	request->baseUrl; ?>/assets-frontend/ico/apple-touch-icon-57-precomposed.png">
 </head>
 
 <body >
